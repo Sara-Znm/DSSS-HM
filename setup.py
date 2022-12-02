8c65c7c3d6c6ad86af7037853dec198c09b17819
Sara-Znm <Sara_Znm>
2022-12-02 19:12:13 +0100

    create requirement , setup, test file for paython packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="snowflake",
+    version="0.1",
+    author="Sara_Znm",
+    author_email="sara.zarifi@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy", "turtle"],
+)
